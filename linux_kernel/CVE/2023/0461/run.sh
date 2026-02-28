be5b41f0ee0aab07e4c15d9889d1d30d5c0a2176
gimmia <gimm78064@gmail.com>
2026-02-28 22:59:54 +0900

    [ADD] exploit

---
+++ linux_kernel/CVE/2023/0461/run.sh
@@ -0,0 +1,13 @@
+qemu-system-x86_64 \
+    -enable-kvm \
+    -cpu qemu64,+smep,+smap,+rdrand \
+    -smp 2 \
+    -m 4G \
+    -kernel ./bzImage \
+    -initrd ./rootfs.cpio.gz \
+    -append "console=ttyS0 kgdbwait kgdboc=ttyS1,115200 oops=panic panic=-1 pti=on quiet kaslr" \
+    -netdev user,id=mynet0 \
+    -device virtio-net-pci,netdev=mynet0 \
+    -gdb tcp:localhost:1234 \
+    -nographic \
+    -no-reboot \
